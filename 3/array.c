cbaf12deffc027694002635d85ff87e3bf398842
Rujian <roman.2006.08.18@gmail.com>
2025-01-25 18:44:37 +0500

    add task

--- 3/array.c
+++ 3/array.c
@@ -1,6 +1,11 @@
 #include "array.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool is_even(int num){
+    return !(num % 2);
+}
 
 int create_new_array(Array *arr, int size){
     int *temp_ptr = (int*)calloc(size, sizeof(int));
@@ -52,6 +57,28 @@ int delete_element(Array *arr, int index){
 }
 
 void subsequence_stuff(Array *arr){
+    int w = 0; // write index
+    for(int i = 0; i < arr->size;){
+        int sum = arr->ptr[i]; // take the first element
+        int parity = arr->ptr[i] % 2;
+        int j = i + 1; // next element index
+
+        // accumulate while parity is the same
+        while(j < arr->size && (arr->ptr[j] % 2) == parity){
+            sum += arr->ptr[j];
+            j++;
+        }
+
+        // if we have at least 2 elements with the same parity
+        if((j - i) >= 2){
+            arr->ptr[w++] = sum;
+        } else {
+            // if only one element, copy it
+            arr->ptr[w++] = arr->ptr[i];
+        }
+        i = j; // skip to the next segment
+    }
+    arr->size = w; // update the array size
 }
 
 void print(Array *arr){
